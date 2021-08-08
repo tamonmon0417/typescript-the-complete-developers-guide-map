0d02ff3866f360a556d8dd9341352d2890922fdc
tamonmon0417 <tamonmon0417@gmail.com>
2021-08-08 15:16:11 +0900

    add user marker

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 /// <reference types="@types/google.maps" />
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  }
-});
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
